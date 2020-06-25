e31b0983f445aa86f3187cc2edbbebea9d92c03b
Shivaadith Anbarasu <shivaadith@gmail.com>
2020-06-26 01:28:39 +0530

    Update dictionary_dtbse.py

--- dictionary_app/dictionary_dtbse.py
+++ dictionary_app/dictionary_dtbse.py
@@ -1,11 +1,9 @@
 import mysql.connector
 from spellchecker import SpellChecker
 
-# creates a spellchecker object
+
 spell = SpellChecker()
 
-
-# mysql.conector.connect(...) establishes a connection to the MySQL serverand returns an object
 
 connect = mysql.connector.connect(
 	user="ardit700_student",
@@ -16,13 +14,6 @@ connect = mysql.connector.connect(
 
 cursor = connect.cursor()
 
-# """
-# 	replacing * by Definition returns only the definition coloum values for the
-# 	expression. No need for using return[1] in the print statement since that is
-# 	neccessary only to avoid printing the expression along with the meaning
-# 	* returns all including the expression itself
-# 	Refer to More SQL Statements section in the Udemy pyhton ARDIT course
-# 	for the various statements that could be used within cursor.execute()
 
 while True:
     word = input("\nEnter a word: ")
@@ -30,12 +21,7 @@ while True:
         query = cursor.execute("SELECT * FROM Dictionary WHERE Expression = '{}'".format(word))
         results = cursor.fetchall()
 
-# 	printing results will print a tuple consisting of the word
-# 	and the appropriate meaning stored in the database.
-# 	Hence a loop can be used to generate the result in a more
-# 	user friendly manner
-# 	result[1] ensures that only the meaning from the definition
-# 	column and not the expression(word) as well
+
 
         if len(results) != 0:
             for result in results:
