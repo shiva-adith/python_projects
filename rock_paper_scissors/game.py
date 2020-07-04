2d410948cb2064d4cc08e4233db90c54b6ae6886
Shivaadith Anbarasu <shivaadith@gmail.com>
2020-07-05 02:43:59 +0530

    Add basic version of game

---
+++ rock_paper_scissors/game.py
@@ -0,0 +1,43 @@
+import random
+
+
+while True:
+    choice = input("Enter your choice: ").lower()
+
+
+
+    choices = ['rock', 'paper', 'scissors']
+
+    # the args for randint set the range required for the output.
+    # the end value is inclusive and hence one is subtracted to prevent going out of index
+    # computer_choice = choices[random.randint(0, len(choices)-1)]
+    computer_choice = random.choice(choices)
+
+    if choice in choices:
+        if choice == computer_choice:
+            print("It's a tie!")
+        if choice == 'rock':
+            if computer_choice == 'paper':
+                print('You lost!')
+            elif computer_choice == 'scissors':
+                print("You beat the Computer!")
+
+        if choice == 'paper':
+            if computer_choice == 'scissors':
+                print('You lost!')
+            elif computer_choice == 'rock':
+                print("You beat the Computer!")
+
+        if choice == 'scissors':
+            if computer_choice == 'rock':
+                print('You lost!')
+            elif computer_choice == 'paper':
+                print("You beat the Computer!")
+    else:
+        print("Incorrect choice, please try again!\n")
+        continue
+
+
+    print('You chose: ', choice)
+    print("The computer chose: ", computer_choice)
+    print()
